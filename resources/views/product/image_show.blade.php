6bd89d9e5790afa6f272f9198c28cfcb96b66c03
kitam936 <tk.ar145qv@gmail.com>
2025-02-13 06:14:27 +0900

    250212_view_adjust

--- resources/views/product/image_show.blade.php
+++ resources/views/product/image_show.blade.php
@@ -1,32 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        画像<br>
+            商品画像<br>
         </h2>
 
         <div class="flex">
 
-        <div class="">
-            <input type="hidden" class="pl-0  ml-0 md:ml-2 w-32 h-6 items-center bg-gray-100 border rounded" name="hinban_id2"  value="{{ $image->hinban_id }}"/>
-            <div class="p-2 w-full ml-12 flex mt-0">
-                <button type="button" onclick="location.href='{{ route('image_index')}}'" class="w-32 h-8 mt-2 md:mt-0 text-white text-sm bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded ">戻る</button>
-            </div>
-        </div>
-
-        <div class="p-2 w-2/3 ml-12 flex mt-0">
-            @if($login_user->role_id <= 2 )
-            <form id="delete_{{$image->hinban_id}}" method="POST" action="{{ route('admin.image_destroy',['hinban'=>$image->hinban_id]) }}">
-                @csrf
-                @method('delete')
-                <div class="ml-0 mt-2 md:ml-4 md:mt-0">
-                    <div class="w-32 h-8 text-center text-sm text-white bg-red-500 border-0 pt-2 px-2 focus:outline-none hover:bg-red-700 rounded ">
-                    <a href="#" data-id="{{ $image->hinban_id }}" onclick="deletePost(this)" >削除</a>
-                    </div>
+            <div class="">
+                <input type="hidden" class="pl-0 ml-0 md:ml-2 w-32 h-6 items-center bg-gray-100 border rounded" name="hinban_id2"  value="{{ $image->hinban_id }}"/>
+                <div class="p-0 w-full ml-6 flex mt-2 md:mt-0">
+                    <button type="button" onclick="window.location.href='{{ url()->previous()}}'" class="w-32 h-8 text-white text-sm bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded ">戻る</button>
+                    <button  onclick="location.href='{{ route('image_index')}}'" class="w-32 h-8 ml-6 text-white text-sm bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded ">画像リスト</button>
                 </div>
-            </form>
-            @endif
-        </div>
-    </div>
+            </div>
+            </div>
+
+            <div class="p-2 w-full ml-4 flex mt-0">
+                @if($login_user->role_id <= 2 )
+                <form id="delete_{{$image->hinban_id}}" method="POST" action="{{ route('admin.image_destroy',['hinban'=>$image->hinban_id]) }}">
+                    @csrf
+                    @method('delete')
+                    <div class="ml-0 mt-2 md:ml-4 md:mt-0">
+                        <div class="w-32 h-8 text-center text-sm text-white bg-red-500 border-0 pt-2 px-2 focus:outline-none hover:bg-red-700 rounded ">
+                        <a href="#" data-id="{{ $image->hinban_id }}" onclick="deletePost(this)" >削除</a>
+                        </div>
+                    </div>
+                </form>
+                @endif
+            </div>
+
 
     </x-slot>
 
@@ -43,17 +45,38 @@
         </div>
     </div>
 
-    <div class="flex flex-wrap">
+    {{-- <div class="flex flex-wrap">
         @foreach ($sku_images as $image )
         <div class="w-1/3 md:w-1/4 p-2 md:p-4">
-        {{-- <a href="{{ route('image.edit',['image'=>$image->id]) }}"> --}}
 
             <div class="text-gray-700"> Col:　{{ $image->col_id  }}</div>
             <x-sku_image-thumbnail :filename="$image->filename"  />
+
+        </div>
+        @endforeach
+    </div> --}}
+
+    <div class="flex flex-wrap">
+        @foreach ($sku_images as $image )
+        <div class="w-1/3 md:w-1/4 p-2 md:p-2">
+        {{-- <a href="{{ route('image.edit',['image'=>$image->id]) }}"> --}}
+
+            <div class="text-gray-700"> Col:　{{ $image->col_id  }}</div>
+            @if(($image->filename))
+            {{-- <a href="{{ route('sku_image_show',['sku'=>$image->sku_id]) }}"> --}}
+            <x-sku_image-thumbnail :filename="$image->filename"  />
+            {{-- </a> --}}
+            @endif
+            @if(!($image->filename))
+            <x-sku_image-thumbnail :filename="$image->filename"  />
+            @endif
+           {{--  <x-sku_image-thumbnail :filename="$image->filename"  />  --}}
         {{-- </a> --}}
         </div>
         @endforeach
     </div>
+
+
 
 
 
